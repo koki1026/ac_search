c660a36b9cca476aff5142b50ac414b65e6b7b29
koki-22 <amakou2626@gmail.com>
2023-11-27 14:34:37 +0900

    feat: randam_ship's number 船の個数を可変にし、実行結果としてすべての船の軌跡をプロットできるように

--- src/random_force.py
+++ src/random_force.py
@@ -11,11 +11,8 @@ class MovingPoint:
         self.angular_velocity = 0
 
     def update_position(self, time_interval):
-        # 速度と角速度から新しい座標を計算
         delta_x = self.speed * math.cos(math.radians(self.angular_velocity)) * time_interval
         delta_y = self.speed * math.sin(math.radians(self.angular_velocity)) * time_interval
-
-        # 新しい座標に移動
         self.x += delta_x
         self.y += delta_y
 
@@ -32,64 +29,50 @@ class MovingPoint:
         return self.angular_velocity
 
 def calculate_combined_force(current_speed, current_angle_degrees):
-    # 現在の速度と角度からベクトルを作成
     current_vector = np.array([current_speed * np.cos(np.radians(current_angle_degrees)),
                                current_speed * np.sin(np.radians(current_angle_degrees))])
-
-    # ランダムな速度と角度を生成
-    random_speed = np.random.uniform(0, 10)  # 0から10までのランダムな速度を生成
-    random_angle_degrees = np.random.uniform(0, 360)  # 0から360までのランダムな角度を生成
-
-    # ランダムな速度と角度からベクトルを作成
+    random_speed = np.random.uniform(0, 10)
+    random_angle_degrees = np.random.uniform(0, 360)
     random_vector = np.array([random_speed * np.cos(np.radians(random_angle_degrees)),
                               random_speed * np.sin(np.radians(random_angle_degrees))])
-
-    # 合成ベクトルを計算（ベクトルの合計）
     combined_vector = current_vector + random_vector
-
     return combined_vector
 
-# 使用例
-point = MovingPoint()
+# 船の数を指定
+num_ships = 5  # 例として5隻の船を指定
 
-# 速度と角速度を設定
-point.set_speed(5)
-point.set_angular_velocity(45)
-# 軌跡の座標を保存するリスト
-trajectory_x = []
-trajectory_y = []
-velocities = []  # 速度のリスト
-angular_velocities = []  # 角速度のリスト
+# 各船の初期位置をランダムに生成
+initial_positions = np.random.rand(num_ships, 2) * 20 - 10  # -10から10の範囲でランダムな座標を生成
+ships = [MovingPoint(x, y) for x, y in initial_positions]
+
+# 軌跡の座標を保存するリスト（各船ごとにリストを用意）
+trajectory_x = [[] for _ in range(num_ships)]
+trajectory_y = [[] for _ in range(num_ships)]
+
+# 各船ごとに速度と角速度のリストを用意
+velocities = [[] for _ in range(num_ships)]
+angular_velocities = [[] for _ in range(num_ships)]
 
 # 時間ごとに更新
 for _ in range(10):
-    point.update_position(5)  # 5秒ごとに更新
-    # 関数を呼び出して合力を計算
-    result = calculate_combined_force(point.get_speed(), point.get_angular_velocity())
-    point.set_speed(np.linalg.norm(result))
-    point.set_angular_velocity(np.degrees(np.arctan2(result[1], result[0])))
-    print(f"Current Position: ({point.x}, {point.y})")
-    trajectory_x.append(point.x)
-    trajectory_y.append(point.y)
-    velocities.append(point.speed)
-    angular_velocities.append(point.angular_velocity)
+    for i in range(num_ships):
+        ships[i].update_position(5)
+        result = calculate_combined_force(ships[i].get_speed(), ships[i].get_angular_velocity())
+        ships[i].set_speed(np.linalg.norm(result))
+        ships[i].set_angular_velocity(np.degrees(np.arctan2(result[1], result[0])))
+        trajectory_x[i].append(ships[i].x)
+        trajectory_y[i].append(ships[i].y)
+        velocities[i].append(ships[i].speed)
+        angular_velocities[i].append(ships[i].angular_velocity)
     time.sleep(1)
 
 # 軌跡をプロット
-plt.plot(trajectory_x, trajectory_y, marker='o', label='Moving Point')
-
-# 各点での速度と角速度のベクトルを矢印でプロット
-for i in range(len(trajectory_x)):
-    plt.arrow(
-        trajectory_x[i], trajectory_y[i],
-        velocities[i] * math.cos(math.radians(angular_velocities[i])),
-        velocities[i] * math.sin(math.radians(angular_velocities[i])),
-        head_width=0.5, head_length=0.5, fc='red', ec='red'
-    )
+for i in range(num_ships):
+    plt.plot(trajectory_x[i], trajectory_y[i], marker='o', label=f'Ship {i+1}')
 
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Trajectory of Random Force Walk Point')
+plt.title('Trajectory of Multiple Ships with Random Force')
 plt.legend()
 plt.grid(True)
 plt.show()
