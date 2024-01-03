a13a5180130f76a34340d4e9d7c95a3174c21deb
koki-22 <amakou2626@gmail.com>
2024-01-03 09:05:54 +0900

    feat/create_pkg pure_pursuit

---
+++ vrx_ws/src/pure_pursuit/pure_pursuit/pure_pursuit.py
@@ -0,0 +1,113 @@
+import rclpy
+import math
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Float64
+import numpy as np
+import matplotlib.pyplot as plt
+from visualization_msgs.msg import MarkerArray, Marker
+# wave, goal , my, thruster l,r,lv,rv
+def euler_from_quaternion(quaternion):                                                                   
+        """クオータニオンからオイラー角を計算する関数                                                        
+        Converts quaternion (w in last place) to euler roll, pitch, yaw                                           
+        quaternion = [x, y, z, w]                                                                                 
+        Bellow should be replaced when porting for ROS 2 Python tf_conversions is done.                           
+        """                                                                                                      
+        x = quaternion.x                                                                                          
+        y = quaternion.y                                                                                          
+        z = quaternion.z                                                                                          
+        w = quaternion.w                                                                                          
+                                                                                                                  
+        sinr_cosp = 2 * (w * x + y * z)                                                                           
+        cosr_cosp = 1 - 2 * (x * x + y * y)                                                                       
+        roll      = np.arctan2(sinr_cosp, cosr_cosp)                                                              
+                                                                                                                  
+        sinp  = 2 * (w * y - z * x)                                                                               
+        pitch = np.arcsin(sinp)                                                                                   
+                                                                                                                  
+        siny_cosp = 2 * (w * z + x * y)                                                                           
+        cosy_cosp = 1 - 2 * (y * y + z * z)                                                                       
+        yaw = np.arctan2(siny_cosp, cosy_cosp)                                                                    
+                                         
+        return roll, pitch, yaw
+
+
+class PurePursuit(Node):
+    def __init__(self):
+        super().__init__('pure_pursuit_node')
+
+        self.right_thruster_pub = self.create_publisher(
+            Float64,
+            "/wamv/thrusters/right/thrust",
+            10
+        )
+
+        self.left_thruster_pub = self.create_publisher(
+            Float64,
+            "/wamv/thrusters/left/thrust",
+            10
+        )
+
+        self.collect_imu_sub = self.create_subscription(
+            Imu,
+            '/world/sydney_regatta/model/wamv/link/wamv/imu_wamv_link/sensor/imu_wamv_sensor/imu',
+            self.imu_data_callback,
+            10
+        )
+        self.collect_Pose_sub = self.create_subscription(
+             PoseArray,
+             '/world/sydney_regatta/dynamic_pose/info',
+             self.pose_data_callback,
+             10
+        )
+
+        self.goalPosX = 0.0 #ゴールのx位置を保存
+        self.goalPosY = 0.0 #ゴールのy位置を保存
+        self.myPos = [0.0]*2
+        self.myPos0 = [0.0]*2
+        self.myAng = 0.0 #自分の角度を保存
+        self.myAngOri = [0.0]*4
+        self.buoys_size = 38
+        self.buoys = [[0.0]*2]*self.buoys_size
+        self.goal_index = 0
+
+    def imu_data_callback(self,msg):
+        self.myAngOri[0] = msg.orientation.x
+        self.myAngOri[1] = msg.orientation.y
+        self.myAngOri[2] = msg.orientation.z
+        self.myAngOri[3] = msg.orientation.w
+        angle_ = euler_from_quaternion(msg.orientation)
+        self.myAng = angle_[2]
+        
+    def pose_data_callback(self, msg):
+        self.myPos[0] = msg.poses[self.buoys_size+1].position.x
+        self.myPos[1] = msg.poses[self.buoys_size+1].position.y
+        self.goalPosX = msg.poses[self.goal_index].position.x
+        self.goalPosY = msg.poses[self.goal_index].position.y
+        markers = MarkerArray()
+        markers.markers.append(rviz_marker(self, self.myPos0, "wam_v", 100))
+        for i in range(self.buoys_size):
+            self.buoys[i][0] = msg.poses[i].position.x - self.myPos[0]
+            self.buoys[i][1] = msg.poses[i].position.y - self.myPos[1]
+            distance = np.linalg.norm(self.buoys)
+            radian = math.atan2(self.buoys[i][0], self.buoys[i][1])
+            angle = radian - self.myAng
+            rela_buoy = [0.8]*2
+            rela_buoy[0] = np.cos(angle)*distance
+            rela_buoy[1] = -np.sin(angle)*distance
+            markers.markers.append(rviz_marker(self, rela_buoy, "marker: "+str(i), i))
+
+        self.marker_pub.publish(markers)
+
+       
+def main(args=None):
+    rclpy.init(args=args)
+    navigation_GUI_node = NavigaitonGUI()
+    rclpy.spin(navigation_GUI_node)
+    navigation_GUI_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
