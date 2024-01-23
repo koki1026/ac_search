132cf8891d7a425d5c548757aeecec794b0cb97b
your_email@example.com <abc@example.com>
2024-01-23 16:07:31 +0900

    fix: action_space & render_system

--- cameleon/cameleon-agent/debugger.py
+++ cameleon/cameleon-agent/debugger.py
@@ -1,8 +1,7 @@
-import gym
+import gymnasium as gym
 import cameleon_gym_env
 import time
 
-print(gym.envs.registry.all())
 env = gym.make('Cameleon-v0')
 
 #環境を初期化
@@ -15,6 +14,5 @@ for _ in range(200):
     if done:
         print("Episode finished after {} timesteps".format(_+1))
         env.reset() #環境を初期化
-        break
     #1秒待機
-    time.sleep(1)
+    time.sleep(0.1)
